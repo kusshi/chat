2617c25990e95db8d9f3766b89a688de3d4660d5
kushiro <fragfighter.mswad@gmail.com>
2019-04-29 18:16:16 +0900

    [追加] Ajaxを使用した非同期処理を追加
    * room_create.jsにAjaxを使用した非同期処理部を追加
    * URLセーフな文字列を非同期的にサーバへPOSTする
    * サーバから返った文字列をroomsのTextContentに代入する

--- public/room_create.js
+++ public/room_create.js
@@ -1,7 +1,32 @@
 window.addEventListener('load', () => {
+    let form = document.getElementById('form');
+    let chatroom_name = document.getElementById('chatroom_name');
 
     form.addEventListener('submit', e => {
         console.log('submit.');
+        let rooms = document.getElementById('rooms');
+        // XMLHttpRequestオブジェクト生成
+        let xhr = new XMLHttpRequest();
+
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    rooms.textContent = xhr.responseText;
+                } else {
+                    rooms.textContent = 'error.'
+                }
+            } else {
+                rooms.textContent = 'loading'
+            }
+        };
+
+        xhr.open('post', '/create_chatroom', true);
+        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        xhr.send('name=' + encodeURIComponent(chatroom_name.value));
+        console.log(chatroom_name.value)
+
+
         e.preventDefault();
     });
 });
