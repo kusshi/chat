7c7fd0b7b90e8c1435e0b5adbce01c43739bd04a
kushiro <fragfighter.mswad@gmail.com>
2019-05-04 20:30:01 +0900

    [追加] JSONで受け取った生成済みチャットルーム一覧をリンクのリストとして積み上げる処理を追加
    * window.addEventListener('load'.....のタイミングでXMLHttpRequestオブジェクトを生成、'/list_chatrooms'にリクエストを投げ、返ってきた生成済みチャットルーム一覧のJSONをparseしてリンクのリストを作成する処理を追加した

--- public/room_create.js
+++ public/room_create.js
@@ -3,6 +3,39 @@ window.addEventListener('load', () => {
     let chatroom_name = document.getElementById('chatroom_name');
     let chatrooms = document.getElementById('chatrooms');
 
+
+    let xhr_first_load = new XMLHttpRequest();
+
+
+    xhr_first_load.onreadystatechange = function () {
+        if (xhr_first_load.readyState === 4) {
+            if (xhr_first_load.status === 200) {
+                let json_received = JSON.parse(xhr_first_load.responseText);
+                for (value of json_received["chat_rooms"]) {
+                    let li = document.createElement('li');
+                    let link_chatroom = document.createElement('a');
+                    let text = document.createTextNode(value["room_name"]);
+                    link_chatroom.href = location.origin + '/chat/' + value["room_url"];
+                    link_chatroom.appendChild(text);
+                    li.appendChild(link_chatroom);
+                    li.classList.add("list-group-item")
+                    chatrooms.insertBefore(li, chatrooms.firstChild);
+                }
+            } else {
+                console.log('error.')
+            }
+        } else {
+            console.log('loading.')
+        }
+    };
+
+    xhr_first_load.open('post', '/list_chatrooms', true);
+    xhr_first_load.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    xhr_first_load.send(null);
+    console.log(chatroom_name.value)
+
+
+
     form.addEventListener('submit', e => {
         console.log('submit.');
         let rooms = document.getElementById('rooms');
